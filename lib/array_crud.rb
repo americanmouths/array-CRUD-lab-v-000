83bb0eb90b759f4184b77fb18bd2e60f2fca6e97
Kayla Hansen <shiverr@gmail.com>
2017-10-10 03:51:21 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  
+  []
 end
-
-def create_an_array
   
+def create_an_array
+  ["Hello", "How're you?", "Good you?", "Good"]
 end
 
 def add_element_to_end_of_array(array, element)
